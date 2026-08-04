d37ea264f2326fa88cedf6ebd9abb199f8f37595
chechelnitzky <chechelnitzky@gmail.com>
2026-08-04 18:49:12 -0400

    Clear timer notification when app is reopened

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import { startOfflineSync } from './lib/offline';
+import { startTimerNotificationLifecycle } from './lib/timerAlerts';
 import './styles.css';
 import './offline.css';
 import './features.css';
@@ -10,5 +11,6 @@ import './selected-date-exercises.css';
 
 registerSW({ immediate: true });
 startOfflineSync();
+startTimerNotificationLifecycle();
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
