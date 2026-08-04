33e445e051cccfe021477fe0525bc884661bd988
chechelnitzky <chechelnitzky@gmail.com>
2026-08-04 16:44:32 -0400

    Start automatic offline synchronization

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
+import { startOfflineSync } from './lib/offline';
 import './styles.css';
 
 registerSW({ immediate: true });
+startOfflineSync();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
